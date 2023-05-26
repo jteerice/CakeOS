d95b2a6de84f6d15ba9e4978322c3a96b59f7576
jteerice <jake.rice@trojans.dsu.edu>
2023-05-26 14:44:08 -0700

    path validator finished

--- src/fs/pparser.c
+++ src/fs/pparser.c
@@ -1,6 +1,12 @@
 #include "pparser.h"
+#include "kernel.h"
+#include "string/string.h"
+#include "memory/heap/kheap.h"
+#include "memory/memory.h"
 
 static int path_valid_format(const char* filename) {
 
-	int len = strlen(filename, CAKEOS_MAX_PATH);
+	int len = strnlen(filename, CAKEOS_MAX_PATH);
+
+	return (len >= 3 && isdigit(filename[o]) && memcmp((void*)&filename[1], ":/", 2) ==0);
 }
--- src/string/string.h
+++ src/string/string.h
@@ -1,6 +1,10 @@
 #ifndef STRING_H
 #define STRING_H
+#include <stdbool.h>
 
 int strlen(const char* str);
+bool isdigit(char c);
+int tonumericdigit(char c);
+int strnlen(const char* str, int max);
 
 #endif
